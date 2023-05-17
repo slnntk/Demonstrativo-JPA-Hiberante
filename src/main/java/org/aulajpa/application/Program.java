ed6f119756a4c14f8082b99e6540d5e32fbb4c61
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-05-17 15:03:30 -0300

    search a person with id

--- src/main/java/org/aulajpa/application/Program.java
+++ src/main/java/org/aulajpa/application/Program.java
@@ -12,10 +12,23 @@ public class Program {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
 
+        /* conectou, criou e inseriu pessoa.
         em.getTransaction().begin(); // precisa começar a transação
+
         em.persist(new Pessoa(null, "Carlos da Silva", "carlos@gmail.com"));
         em.persist(new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com"));
         em.persist(new Pessoa(null, "Ana Maria", "ana@gmail.com"));
+
         em.getTransaction().commit(); // confirmar transação
+        */
+
+        /* buscar uma pessoa no bd
+        Pessoa p = em.find(Pessoa.class, 2);
+        System.out.println(p);
+        */
+        
+        em.close();
+        emf.close();
+        System.out.println("Pronto!");
     }
 }
